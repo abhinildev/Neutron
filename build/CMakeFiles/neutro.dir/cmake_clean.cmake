755ab4fa62b7fd74765f6fd5b1b7a8f52cfbdb8c
abhinildev <savarniabhinil@gmail.com>
2026-08-20 11:28:14 +0530

    Commit1

---
+++ build/CMakeFiles/neutro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neutro.dir/link.d"
+  "CMakeFiles/neutro.dir/src/main.cpp.o"
+  "CMakeFiles/neutro.dir/src/main.cpp.o.d"
+  "neutro"
+  "neutro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neutro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
